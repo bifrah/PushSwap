fec743c594bcf30794c9024fe64bffb98402324f
bifrah <benj.ifrah@gmail.com>
2021-12-11 16:31:22 +0100

    ajout define et include de base

---
+++ include/push_swap.h
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/11 16:24:45 by bifrah            #+#    #+#             */
+/*   Updated: 2021/12/11 16:31:08 by bifrah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP
+# define PUSH_SWAP
+
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+# include <limits.h>
+
+#define MALLOC_ERROR -2
+#define ERROR		 -3
+#define INPOUT_ERROR -4
+#define BLABLA_ERROR -5
+
+#endif
