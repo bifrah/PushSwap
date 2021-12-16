a40e5f11ef01bc4f8822c2695e4e0e06a8f960fd
bifrah <benj.ifrah@gmail.com>
2021-12-16 18:37:15 +0100

    checkdouble ok

--- srcs/checks.c
+++ srcs/checks.c
@@ -6,7 +6,7 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/16 16:58:09 by bifrah            #+#    #+#             */
-/*   Updated: 2021/12/16 17:50:54 by bifrah           ###   ########.fr       */
+/*   Updated: 2021/12/16 18:33:08 by bifrah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,18 +17,24 @@ int	ft_checkdouble(int *tab, int size)
 	int	i;
 	int	j;
 
-	i = size;
-	j = size - 1;
+	i = size - 1;
+	j = i - 1;
 	print_tab(tab, size);
+	printf("size : %d\n", size);
 	while (i >= 0)
 	{
 		while (i >= 0 && j >= 0)
 		{
-			if (tab[i] == tab[j])
+			printf("tab i : %d\n", tab[i]);
+			printf("tab j : %d\n\n", tab[j]);
+			if (tab[j] == tab[i])
+			{
 				return (-1);
+			}
 			j--;
 		}
 		i--;
+		j = i - 1;
 	}
 	return (0);
 }
