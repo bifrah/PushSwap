de18125e4b7f4728c01300eca1bd8efc2a1ff332
Benjamin <benj.ifrah@gmail.com>
2021-12-20 21:18:58 +0100

    sa sb & ss ok

--- srcs/operations.c
+++ srcs/operations.c
@@ -1,19 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   operations.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/20 21:18:40 by bifrah            #+#    #+#             */
+/*   Updated: 2021/12/20 21:18:41 by bifrah           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../include/push_swap.h"
 
 void	sa(long int **tab_a, int argc, int message)
 {
-	
+	int			i;
+	long int	tmp;
+
+	i = 0;
+	if (argc > 2)
+	{
+		tmp = tab_a[0][i];
+		tab_a[0][i] = tab_a[0][i + 1];
+		tab_a[0][i + 1] = tmp;
+	}
+	if (message != 0)
+		ft_putstr_fd("sa\n", 1);
 }
 
 void	sb(long int **tab_b, int argc, int message)
 {
-	
+	int			i;
+	long int	tmp;
+
+	i = 0;
+	if (argc > 2)
+	{
+		tmp = tab_b[0][i];
+		tab_b[0][i] = tab_b[0][i + 1];
+		tab_b[0][i + 1] = tmp;
+	}
+	if (message != 0)
+		ft_putstr_fd("sb\n", 1);
 }
 
 void	ss(long int **tab_a, long int **tab_b, int argc)
 {
-	sa();
-	sb();
+	sa(&tab_a, argc, 0);
+	sb(&tab_b, argc, 0);
+	ft_putstr_fd("ss\n", 1);
 }
 
 void	pa(long int **tab_a, long int **tab_b, int argc)
