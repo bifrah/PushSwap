fe62662a6c7d45a5a89031be9eb6364fae110258
Benjamin <benj.ifrah@gmail.com>
2021-12-20 21:10:22 +0100

    base

--- srcs/operations.c
+++ srcs/operations.c
@@ -1,1 +1,59 @@
 #include "../include/push_swap.h"
+
+void	sa(long int **tab_a, int argc, int message)
+{
+	
+}
+
+void	sb(long int **tab_b, int argc, int message)
+{
+	
+}
+
+void	ss(long int **tab_a, long int **tab_b, int argc)
+{
+	sa();
+	sb();
+}
+
+void	pa(long int **tab_a, long int **tab_b, int argc)
+{
+	
+}
+
+void	pb(long int **tab_a, long int **tab_b, int argc)
+{
+	
+}
+
+void	ra(long int **tab_a, int argc, int message)
+{
+	
+}
+
+void	rb(long int **tab_b, int argc, int message)
+{
+	
+}
+
+void	rr(long int **tab_a, long int **tab_b, int argc)
+{
+	ra();
+	rb();
+}
+
+void	rra(long int **tab_a, long int **tab_b, int argc, int message)
+{
+	
+}
+
+void	rrb(long int **tab_a, long int **tab_b, int argc, int message)
+{
+	
+}
+
+void	rrr(long int **tab_a, long int **tab_b, int argc)
+{
+	rra();
+	rrb();
+}
