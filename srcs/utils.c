df3fd1bfb1f729a4face72e7400b367625e64707
Benjamin <benj.ifrah@gmail.com>
2021-12-20 20:58:28 +0100

    suppression fct inutile

--- srcs/utils.c
+++ srcs/utils.c
@@ -6,7 +6,7 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/11 17:00:48 by bifrah            #+#    #+#             */
-/*   Updated: 2021/12/17 17:36:56 by bifrah           ###   ########.fr       */
+/*   Updated: 2021/12/20 20:58:18 by bifrah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,27 +17,4 @@ int	ft_issign(int c)
 	if (c == '-' || c == '+')
 		return (1);
 	return (0);
-}
-
-int	ptrstrlen(char **dest)
-{
-	int	j;
-
-	j = 0;
-	while (dest[j])
-		j++;
-	if (j == 1 && dest[0][0] == '\0')
-		return (0);
-	return (j);
-}
-
-void	ft_freetab(long int **tab)
-{
-	int	y;
-
-	y = -1;
-	while (tab[++y])
-		free(tab[y]);
-	free(*tab);
-	*tab = NULL;
 }
