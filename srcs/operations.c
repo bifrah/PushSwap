4c8b97cacfab6afdb6969e501c5c3c6e62392817
Benjamin <benj.ifrah@gmail.com>
2021-12-24 14:13:32 +0100

    segfault quand utilisation de *size

--- srcs/operations.c
+++ srcs/operations.c
@@ -6,7 +6,7 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/20 21:18:40 by bifrah            #+#    #+#             */
-/*   Updated: 2021/12/24 12:52:06 by bifrah           ###   ########.fr       */
+/*   Updated: 2021/12/24 14:01:48 by bifrah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -142,28 +142,39 @@ void	rr(long int **tab_a, long int **tab_b, t_param *param)
 	ft_putstr_fd("rr\n", 1);
 }
 
-void	rrtab(long int **tab, t_param *param, int message)
+void	rrtab(long int **tab, t_param *param, int sizetab, int message)
 {
 	int			i;
 	long int	tmp;
+	int			*size;
 
-	i = param->argc - 1;
-	tmp = tab[0][i];
-	while (i - 1 >= 0)
+	size = 0;
+	if (sizetab == 1)
+		size = &(param->size_a);
+	if (sizetab == 2)
+		size = &(param->size_b);
+	printf("size : %d\n", *size);
+	write(1, "asdf\n", 5);
+	if (*size > 0)
 	{
-		tab[0][i] = tab[0][i - 1];
-		i--;
+		i = *size - 1;
+		tmp = tab[0][i];
+		while (i - 1 >= 0)
+		{
+			tab[0][i] = tab[0][i - 1];
+			i--;
+		}
+		tab[0][i] = tmp;
+		if (message == 1)
+			ft_putstr_fd("rra\n", 1);
+		if (message == 2)
+			ft_putstr_fd("rrb\n", 1);
 	}
-	tab[0][i] = tmp;
-	if (message == 1)
-		ft_putstr_fd("rra\n", 1);
-	if (message == 2)
-		ft_putstr_fd("rrb\n", 1);
 }
 
 void	rrr(long int **tab_a, long int **tab_b, t_param *param)
 {
-	rrtab(tab_a, param, 0);
-	rrtab(tab_b, param, 0);
+	rrtab(tab_a, param, 1, 0);
+	rrtab(tab_b, param, 2, 0);
 	ft_putstr_fd("rrr\n", 1);
 }
