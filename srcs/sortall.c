997571d11588a3c8764bc515b82cb31031c2015d
bifrah <benj.ifrah@gmail.com>
2022-01-05 13:43:47 +0100

    suppression commentaire

--- srcs/sortall.c
+++ srcs/sortall.c
@@ -6,7 +6,7 @@
 /*   By: bifrah <bifrah@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/17 16:53:45 by bifrah            #+#    #+#             */
-/*   Updated: 2022/01/05 11:42:44 by bifrah           ###   ########.fr       */
+/*   Updated: 2022/01/05 13:43:34 by bifrah           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,16 +73,3 @@ void	ft_sortall(t_param *param, long int **tab_a, long int **tab_b)
 			pa(param, tab_a, tab_b);
 	}
 }
-
-/*
-	while (tab_a[i] > tab_a[i + 1] && tab_b[i] > tab_b[i + 1])
-	{
-		while (param->size_a / 2 > 0)
-			pb(param, tab_a, tab_b);
-		ss(param, tab_a, tab_b);
-		while (tab_a[i] > tab_a[i + 1])
-			sa(param, tab_a, 1);
-		while (tab_b[i] > tab_b[i + 1])
-			sb(param, tab_b, 1);
-	}
-*/ 
